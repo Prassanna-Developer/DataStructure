4d98166b1f1517da0139fd810d45e24f846b4146
Prassanna , Java Developer , OCJP Certified, CSM Certified <iamprassanna@gmail.com>
2016-06-28 00:00:34 +0530

    Updated code
    00-44  48 5555 8361 should convert to 004-448-555-583-61
    
    0 - 22 1985--324 should convert to 022-198-53-24
    
    555372654 should convert to 555-372-654

--- Problem_Solving/RegexSolution.java
+++ Problem_Solving/RegexSolution.java
@@ -1,7 +1,7 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Solution {
+public class RegexSolution {
 
 	public static void main(String[] args) {
 		String s = "";
